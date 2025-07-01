f9226b2022fdc277323eab0ad59784b76198cd7c
Braedan Royer <royerbraedan@gmail.com>
2025-06-30 20:59:27 -0700

    added a tweet btn event listener that currently logs out the value of the text area

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 import { tweetsData } from './data.js'
 
-console.log(tweetsData)
+/* Dom Elements */
+const tweetInput = document.querySelector('#tweet-input')
+const tweetBtn = document.querySelector('#tweet-btn')
+
+/* Event listeners */
+tweetBtn.addEventListener('click', () => {
+  console.log(tweetInput.value)
+  tweetInput.value = ''
+})
